aa087febeee88dcf425de60b9ca3d41a5fad4056
Melissa Danielson <59889711+melissamae1@users.noreply.github.com>
2020-01-31 13:14:44 -0600

    My File

--- app.py
+++ app.py
@@ -1,14 +1,44 @@
-from flask import Flask, render_template
+import pymysql.cursors
+
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
 
-@app.route('/cakes')
-def cakes():
-    return 'Yummy cakes!'
+    # Connect to the database
+    connection = pymysql.connect(host='mrbartucz.com',
+                                 user='lg6757bu',
+                                 password='password',
+                                 db='lg6757bu_University',
+                                 charset='utf8mb4',
+                                 cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with connection.cursor() as cursor:
+
+            #search by user input
+            name = input("Enter a first name to search: ")
+            sql = "SELECT * from Students WHERE firstName LIKE %s"
+
+            # execute the SQL command
+            cursor.execute(sql, (name,))
+
+            #data = cursor.fetchall()
+
+            #result = dict(data)
+                
+            # get the results
+            for result in cursor:
+                print (result)
+                
+                
+            
+    finally:
+        connection.close()
+    
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
